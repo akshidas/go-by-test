45b93589501b34de8e044bb94004252c6178b2ae
Akshay <108661354+akshidas@users.noreply.github.com>
2024-09-26 19:34:18 +0530

    learned about maps

--- 07_maps/main.go
+++ 07_maps/main.go
@@ -3,18 +3,20 @@ package main
 type Dictionary map[string]string
 
 const (
-	ErrUnknownKey   = DictionaryError("could not find the word you were looking for")
-	ErrDuplicateKey = DictionaryError("could not add the key as duplicate exists")
+	ErrUnknownKey     = DictionaryErr("the key requested is not found")
+	ErrDuplicateKey   = DictionaryErr("the key is already present")
+	ErrFailedToUpdate = DictionaryErr("failed to update")
 )
 
-type DictionaryError string
+type DictionaryErr string
 
-func (e DictionaryError) Error() string {
+func (e DictionaryErr) Error() string {
 	return string(e)
 }
 
 func (d Dictionary) Search(key string) (string, error) {
 	value, ok := d[key]
+
 	if ok {
 		return value, nil
 	}
@@ -24,7 +26,6 @@ func (d Dictionary) Search(key string) (string, error) {
 
 func (d Dictionary) Add(key, value string) error {
 	_, err := d.Search(key)
-
 	switch err {
 	case ErrUnknownKey:
 		d[key] = value
--- 07_maps/main_test.go
+++ 07_maps/main_test.go
@@ -1,55 +1,78 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
-	d := Dictionary{"test": "this is test"}
+	dictionary := Dictionary{"test": "This is test"}
 
-	t.Run("searching known key", func(t *testing.T) {
-		got, _ := d.Search("test")
-		want := "this is test"
-		assertString(t, want, got)
+	t.Run("known key", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "This is test"
+		assertString(t, got, want)
 	})
 
-	t.Run("searching unknown key", func(t *testing.T) {
-		_, got := d.Search("ttest")
-		want := ErrUnknownKey
-		assertError(t, want, got)
-		assertString(t, want.Error(), got.Error())
+	t.Run("unknown key", func(t *testing.T) {
+		_, err := dictionary.Search("test_test")
+		assertError(t, err)
+		assertString(t, err.Error(), ErrUnknownKey.Error())
 	})
 }
 
 func TestAdd(t *testing.T) {
-	d := Dictionary{}
+	dictionary := Dictionary{}
 
-	t.Run("Add a new key", func(t *testing.T) {
-		got := d.Add("test", "this is test")
-
-		if got != nil {
-			t.Error("Wanted Error to be nil but got an error")
-		}
+	t.Run("Add a value", func(t *testing.T) {
+		dictionary.Add("test", "This is test")
+		got, _ := dictionary.Search("test")
+		assertString(t, got, "This is test")
 	})
 
-	t.Run("Add a duplicate key", func(t *testing.T) {
-		got := d.Add("test", "this is test")
-		want := ErrDuplicateKey
-
-		assertError(t, want, got)
-		assertString(t, want.Error(), got.Error())
+	t.Run("Failed to  add a duplicate value", func(t *testing.T) {
+		err := dictionary.Add("test", "This is test")
+		assertError(t, err)
+		assertString(t, err.Error(), ErrDuplicateKey.Error())
 	})
 
 }
 
-func assertError(t testing.TB, want, got error) {
+func TestUpdate(t *testing.T) {
+	dictionary := Dictionary{"test": "this is test"}
+	t.Run("update a known key", func(t *testing.T) {
+		dictionary.Update("test", "this is test updated")
+		got, _ := dictionary.Search("test")
+		want := "this is test updated"
+		assertString(t, want, got)
+	})
+
+	t.Run("update unknown key", func(t *testing.T) {
+		got := dictionary.Update("ttest", "this is test updated")
+		want := ErrFailedToUpdate
+		assertError(t, got)
+		assertString(t, want.Error(), got.Error())
+	})
+}
+
+func TestDelete(t *testing.T) {
+	dictionary := Dictionary{"test": "this is test"}
+	dictionary.Delete("test")
+	_, got := dictionary.Search("test")
+	want := ErrUnknownKey
+	assertError(t, got)
+	assertString(t, want.Error(), got.Error())
+}
+
+func assertError(t testing.TB, err error) {
 	t.Helper()
-	if got == nil {
-		t.Fatalf("Want error to be %v but got %v", want, got)
+	if err == nil {
+		t.Fatalf("Expected error %s but didn't get one", ErrUnknownKey.Error())
 	}
 }
 
-func assertString(t testing.TB, want, got string) {
+func assertString(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("Want %s but got %s", want, got)
+		t.Errorf("Got %s but want %s", got, want)
 	}
 }
